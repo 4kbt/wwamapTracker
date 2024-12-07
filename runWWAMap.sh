730b0d0597d8aa3450c43cdc0eb9b40de04cf287
Charlie Hagedorn <charlie@hagedorn.xyz>
2024-12-06 22:40:36 -0800

    Auto Server Commit 06.12.2024 um 22:40

--- runWWAMap.sh
+++ runWWAMap.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+cd /home/charlie/wwamapTracker/
+
 DATE=`date -Iseconds`
 
 URL="https://www.weather.gov/wwamap/png/US.png"
@@ -7,3 +9,13 @@ URL="https://www.weather.gov/wwamap/png/US.png"
 FN="data/${DATE}_wwamapUS.png"
 
 curl $URL > $FN
+
+git stage data/*
+
+timestamp(){
+    date +"%d.%m.%Y um %H:%M"
+}
+
+git commit -am "Auto Server Commit $(timestamp)"
+
+git push --repo https://4kbt:`cat wwaAccessToken.txt`@github.com/4kbt/trmnlNWAC.git 

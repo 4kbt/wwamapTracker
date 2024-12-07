fb65edbfcd887c2a4fe9d53284efa2b38a75255a
Charlie Hagedorn <charlie@hagedorn.xyz>
2024-12-07 08:48:06 -0800

    Adds a makeMovie script for easy timelapses. Updates gitignore to match

---
+++ makeMovie.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+OUT="timelapse.mp4"
+
+rm $OUT
+
+# This depends upon cat properly ordering the files, which it generally should.
+# cat may not handle large numbers of files appropriately -- a fix for a
+# future time.
+
+cat data/*.png | ffmpeg -f image2pipe -framerate 5 -i - $OUT
